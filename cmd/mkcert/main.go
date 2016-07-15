c86f0e7691d8a28c3d2cedfa289b1fb05163d3b4
Joe Kopena <tjkopena@bellerophonmobile.com>
2016-07-14 23:07:19 -0400

    Report parse errors.

--- cmd/mkcert/main.go
+++ cmd/mkcert/main.go
@@ -30,7 +30,7 @@ func (c claims) Set(flag string) error {
 
 	tok, err := dec.Token()
 	if err != nil {
-		return fmt.Errorf("Invalid claim value format")
+		return fmt.Errorf("Invalid claim value format: ", err)
 	}
 
 	switch tok := tok.(type) {
